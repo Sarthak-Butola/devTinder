2d1eaf3efddc63912e8e8820eac0f39a8e8c1a6e
Sarthak Butola <sarthakbutola@gmail.com>
2025-01-15 16:47:18 +0530

    error handlers basics-1

--- src/app.js
+++ src/app.js
@@ -3,47 +3,24 @@ const app = express();
 
 
 
-app.get("/test",(req,res)=>{
-    res.send("got Namaste test!!");
-});
-
-app.post("/test",(req,res)=>{
-    res.send("posted Namaste test!!");
-});
-
-app.put("/test",(req,res)=>{
-    res.send("Putted Namaste test!!");
-});
-
-app.delete("/test",(req,res)=>{
-    res.send("Deleted Namaste test!!");
-});
-
-//this responds to all types get,put,delete,post etc so write below
-app.use("/test",(req,res)=>{
-    res.send("Namaste test!!");
-});
-
-app.use("/hello/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    const {userId, name, password} = req.params;
-res.send("Namaste Boi Hello.." + ` userId:${userId}, name:${name}, pass:${password}`);
-});
-
-app.use("/hello/123",(req,res)=>{
-    res.send("Namaste Boi Hello 123!! ");
-});
-
-app.use("/hello",(req,res)=>{
+app.use("/hello",(req,res,next)=>{
+    throw new Error("hi");
     res.send("Namaste Boi Hello!!");
-});
+    console.log('go to next');
+}
+);
+app.use("/hell",(req,res)=>{
+    throw new Error("hi");
+    res.send("response sent");
+})
+
+//if any error occurs in any route then this msg is sent
+app.use("/",(err,req,res,next)=>{
+    if(err)
+    res.send("something went wrong");
+})
 
 
-
-
-app.use("/" , (req,res)=>{
-    res.send("Namaste Boi!!");
-});
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening to port 7777");
